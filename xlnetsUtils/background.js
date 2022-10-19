3698f56e59441de534956bcaddfdd726733fcdb7
Javier Alvarez <javier.alvarez.izquierdo@gmail.com>
2022-10-19 16:18:21 +0200

    Se extiende la funcionalidad para el entorno de pruebas

--- xlnetsUtils/background.js
+++ xlnetsUtils/background.js
@@ -1,9 +1,16 @@
 function xlnetsUtils(){	
+	let domain = window.location.hostname;
 	let xlnetsLoginJSP = 'N38LoginEuskadi.jsp';
 	let xlnetsLoginInfoJSP = 'N38LoginInf.jsp';
+	let hostNameDesContains = '.des.';
+	let hostNamePruContains = '.pru.';
 	let url = document.location.href;
-	if(url.indexOf(xlnetsLoginJSP) > -1){		
-		document.cookie = "gogoratuAukeraadfs-1=ukatu; path=/; domain=xlnets.servicios.des.ejgv.euskalsarea.eus";	
+	if(url.indexOf(xlnetsLoginJSP) > -1){	
+		if(domain.indexOf(hostNameDesContains) > -1){
+			document.cookie = "gogoratuAukeraadfs-1=ukatu; path=/; domain=xlnets.servicios.des.ejgv.euskalsarea.eus";	
+		}else if(domain.indexOf(hostNamePruContains) > -1){
+			document.cookie = "gogoratuAukeraadfs-1=ukatu; path=/; domain=xlnets.servicios.pru.ejgv.euskalsarea.eus";
+		}		
 	}else if(url.indexOf(xlnetsLoginInfoJSP) > -1){		
 		document.getElementById('botizq').click();
 	}
